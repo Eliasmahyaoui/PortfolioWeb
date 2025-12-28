e585cc483c0ee09a200a5aca8458f41161480f7a
Elias Mahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-28 03:52:56 +0100

    Actualizado

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,11 @@
+ 
+//Cogemos el id del boton1 
+const boton = document.getElementById("boton1");
 
+//Hacemos el evento para que haga "algo"
 
+boton.addEventListener('click', function(){
 
+    
 
+});
