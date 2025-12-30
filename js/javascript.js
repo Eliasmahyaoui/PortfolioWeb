c02b9c19280e8bc53a580c51bba2b25e11492ab5
Elias Mahyaoui <eliasmahyaoui6@hotmail.com>
2025-12-30 03:44:49 +0100

    portofolio actualizado casi terminado

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,6 @@
- 
-//Cogemos el id del boton1 
 const boton = document.getElementById("boton1");
 
-//Hacemos el evento para que cuando haga click, haga algo
-
-boton.addEventListener('click', function(){
-
-    this.innerHTML="HAS ENTRADO A MI INFORMACION";
-    
-
+boton.addEventListener("click", () => {
+  document.getElementById("sobremi")
+    .scrollIntoView({ behavior: "smooth" });
 });
-
-
-
-
-
-
